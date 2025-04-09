2c1eda4d7270537a2bd9e0acf8958fab948fbb79
UwimaanaO <uwimaanaolivia2000@gmail.com>
2025-04-09 18:03:55 +0300

    User Dashboard and Admin Dashboard updates

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,4 @@ Route::middleware([
     Route::get('/admin/dashboard', function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
-
-    Route::get('/user/dashboard', function () {
-        return view('user.dashboard');
-    })->name('user.dashboard');
 });
