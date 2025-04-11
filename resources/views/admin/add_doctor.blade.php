25b0a521d180798543f2143807a9d34f03c6a6db
UwimaanaO <uwimaanaolivia2000@gmail.com>
2025-04-11 16:22:57 +0300

    Add doctor form

--- resources/views/admin/add_doctor.blade.php
+++ resources/views/admin/add_doctor.blade.php
@@ -6,81 +6,74 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Admin Page</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="admin/assets/vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="admin/assets/vendors/jvectormap/jquery-jvectormap.css">
-  <link rel="stylesheet" href="admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
-  <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
-  <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <!-- endinject -->
-  <!-- Layout styles -->
-  <link rel="stylesheet" href="admin/assets/css/style.css">
-  <!-- End layout styles -->
-  <link rel="shortcut icon" href="admin/assets/images/favicon.png" />
+  @include('admin.css')
+  <style>
+    label{
+      display: inline-block;
+      width: 200px;
+    }
+    input{
+      width: 200px;
+    }
+  </style>
 </head>
 
 <body>
   <div class="container-scroller">
-    <div class="row p-0 m-0 proBanner" id="proBanner">
-      <div class="col-md-12 p-0 m-0">
-        <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-          <div class="ps-lg-1">
-            <div class="d-flex align-items-center justify-content-between">
-              <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with
-                this template!</p>
-              <a href="https://www.bootstrapdash.com/product/corona-free/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo"
-                target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
-            </div>
-          </div>
-          <div class="d-flex align-items-center justify-content-between">
-            <a href="https://www.bootstrapdash.com/product/corona-free/"><i
-                class="mdi mdi-home me-3 text-white"></i></a>
-            <button id="bannerClose" class="btn border-0 p-0">
-              <i class="mdi mdi-close text-white me-0"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
     <!-- include nav bar page -->
     @include('admin.sidebar')
-    <!-- partial -->
+    <!-- include nav bar -->
+    @include('admin.navbar')
+    <!-- incude body-->
     <div class="container-fluid page-body-wrapper">
-      <!-- include nav bar -->
-      @include('admin.navbar')
-      <!-- incude body-->
-      @include('admin.body')
-      <!-- main-panel ends -->
+      <div class="container" align="center" style="padding-top: 100px">
+        <form action="">
+          @csrf
+          <div style="padding: 15px">
+            <label for="">Doctor's Name</label>
+            <input type="text" name="name" placeholder="Enter Doctor's Name" style="color: black">
+          </div>
+          <div style="padding: 15px">
+            <label for="">Phone Numebr</label>
+            <input type="text" name="phone" placeholder="Enter Doctor's Phone Number" style="color: black">
+          </div>
+          <div style="padding: 15px">
+            <label for="">Doctor's Speciality</label>
+            <select name="speciality" id="" style="width: 200px">
+              <option value="">--Select--</option>
+              <option value="Primary Care">Primary Care</option>
+              <option value="Pediatrics">Pediatrics</option>
+              <option value="Obstetrics and Gynecology (OB/GYN)">Obstetrics and Gynecology (OB/GYN)</option>
+              <option value="Dermatology">Dermatology</option>
+              <option value="Psychiatry">Psychiatry</option>
+              <option value="Neurology">Neurology</option>
+              <option value="Cardiology">Cardiology</option>
+              <option value="Endocrinology">Endocrinology</option>
+              <option value="Gastroenterology">Gastroenterology</option>
+              <option value="Nephrology">Nephrology</option>
+              <option value="Pulmonology">Pulmonology</option>
+              <option value="Rheumatology">Rheumatology</option>
+              <option value="Hematology">Hematology</option>
+              <option value="Oncology">Oncology</option>
+            </select>
+          </div>
+          <div style="padding: 15px">
+            <label for="">Room Numebr</label>
+            <input type="text" name="room" placeholder="Enter Doctor's Room Number" style="color: black">
+          </div>
+          <div style="padding: 15px">
+            <label for="">Doctor's Photo</label>
+            <input type="file" name="photo">
+          </div>
+          <div style="padding: 15px">
+            <input type="submit" class="btn btn-success">
+          </div>
+
+        </form>
+      </div>
     </div>
-    <!-- page-body-wrapper ends -->
   </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="admin/assets/vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="admin/assets/vendors/chart.js/Chart.min.js"></script>
-  <script src="admin/assets/vendors/progressbar.js/progressbar.min.js"></script>
-  <script src="admin/assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
-  <script src="admin/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <script src="admin/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
-  <script src="admin/assets/js/jquery.cookie.js" type="text/javascript"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="admin/assets/js/off-canvas.js"></script>
-  <script src="admin/assets/js/hoverable-collapse.js"></script>
-  <script src="admin/assets/js/misc.js"></script>
-  <script src="admin/assets/js/settings.js"></script>
-  <script src="admin/assets/js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page -->
-  <script src="admin/assets/js/dashboard.js"></script>
-  <!-- End custom js for this page -->
+  @include('admin.script')
 </body>
 
 </html>
