897bbe9fe596082b3976f229f8d018d01e482e8a
UwimaanaO <uwimaanaolivia2000@gmail.com>
2025-04-11 18:37:21 +0300

    Appointments database

---
+++ database/migrations/2025_04_11_153337_create_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->required();
+            $table->string('email')->unique();
+            $table->string('date')->required();
+            $table->string('doctor')->required();
+            $table->string('phone')->required();
+            $table->string('message')->required();
+            $table->string('status')->nullable();
+            $table->string('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
